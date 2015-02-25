0af3e5d32bc466c4ca2ddac230d153ca2e5b7abf
John Josef <john.josef@gmail.com>
2015-02-25 16:44:53 -0500

    tracking api added

--- example/index.js
+++ example/index.js
@@ -8,3 +8,39 @@ var usps = new uspsAPI({
   password: 'PASSWORD',
   imperial: true // set to false for metric
 });
+
+/**
+ * RATES
+ */
+usps.rates({
+  packages: [
+    {
+      Service: 'FIRST CLASS',
+      FirstClassMailType: 'LETTER',
+      ZipOrigination: '44106',
+      ZipDestination: '28262',
+      Pounds: 0,
+      Ounces: 3.5,
+      Container: null,
+      Size: 'REGULAR',
+      Machinable: 'true'
+    }
+  ]
+}, function(err, res) {
+  if(err) {
+    return console.log(util.inspect(err, {depth: null}));
+  }
+
+  console.log(util.inspect(res, {depth: null}));
+});
+
+/**
+ * TRACKING
+ */
+usps.track(['EJ123456780US', '12345'], function(err, res) {
+  if(err) {
+    return console.log(util.inspect(err, {depth: null}));
+  }
+
+  console.log(util.inspect(res, {depth: null}));
+});
